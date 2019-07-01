e0e1ca4274f660917caa72c65f6e945ea5668234
aristofanischionis <aristofanischionis@gmail.com>
2019-07-01 18:09:16 +0300

    some progress in bot

---
+++ BotApplication/CryptographyFunctions.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+# 'private_key.pem'
+# "public_key.pem"
+
+def createPrivateKey(filename):
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+    pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+    with open(filename, 'wb') as f:
+        f.write(pem)
+
+
+def createPublicKey(private_key, filename):
+    public_key = private_key.public_key()
+    pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    with open(filename, 'wb') as f:
+        f.write(pem)
+
+
+def readPublicKey(filename):
+    with open(filename, "rb") as key_file:
+        public_key = serialization.load_pem_public_key(
+            key_file.read(),
+            backend=default_backend()
+        )
+    return public_key
+
+
+def readPrivateKey(filename):
+    with open("private_key.pem", "rb") as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None,
+            backend=default_backend()
+        )
+    return private_key
+
+
+def MyEncrypt(public_key, message):
+    # message = b'encrypt me!'
+    # encode this somehow
+    encrypted = public_key.encrypt(
+        message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return encrypted
+
+
+def MyDecrypt(private_key, encrypted_data):
+    original_message = private_key.decrypt(
+        encrypted_data,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return original_message
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ThesisProject
 Botnet over public blockchain Network.based on the ChainChannels paper.A web app for the bot-master to send instructions, a python script for bots to execute orders by master.
+
+
+start here: http://www.dappuniversity.com/articles/web3-py-intro
